c3a78d814c1003d1ebfdd5485ea2da45ce19364a
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-03-24 06:57:35 +0500

    ui improvement, bug fixing

--- lib/features/additional/teachers/teachers_widgets.dart
+++ lib/features/additional/teachers/teachers_widgets.dart
@@ -16,51 +16,58 @@ class _TeacherWidget extends StatelessWidget {
       ),
       child: Row(
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: SizedBox(
-              height: 70,
-              width: 70,
-              child: CachedNetworkWidget(
-                teacher.avatar,
-              ),
+          SizedBox(
+            width: 70,
+            child: Column(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(12),
+                  child: SizedBox(
+                    height: 70,
+                    width: 70,
+                    child: CachedNetworkWidget(
+                      teacher.avatar,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 5),
+                Text(
+                  teacher.subject,
+                  style: context.smallName,
+                ),
+              ],
             ),
           ),
-          SizedBox(width: 10),
+          SizedBox(width: 8),
           Expanded(
-            child: Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    teacher.fullname,
-                    style: context.name,
-                    maxLines: 2,
-                    textAlign: TextAlign.center,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  teacher.fullname,
+                  style: context.name,
+                  maxLines: 2,
+                ),
+                SizedBox(height: 5),
+                Text(
+                  teacher.email,
+                  style: context.smallName,
+                  textAlign: TextAlign.end,
+                ),
+                SizedBox(height: 5),
+                Container(
+                  padding: EdgeInsets.all(2),
+                  decoration: BoxDecoration(
+                    color: context.selection,
+                    borderRadius: BorderRadius.circular(4),
                   ),
-                  SizedBox(height: 5),
-                  Text(
-                    teacher.email,
-                    style: context.smallName,
-                    textAlign: TextAlign.center,
-                  ),
-                  SizedBox(height: 5),
-                  Text(
+                  child: Text(
                     teacher.officeHours,
                     style: context.smallName,
                     textAlign: TextAlign.center,
                   ),
-                  SizedBox(height: 5),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: Text(
-                      teacher.subject,
-                      style: context.smallName,
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ],
-              ),
+                ),
+              ],
             ),
           ),
         ],
